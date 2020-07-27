246cadca7d12dd93a028cd30510a168bda222b5c
Evan Pavone <evanrpavone@gmail.com>
2020-07-27 20:51:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,13 +7,13 @@ def speak_to_grandma(string = "Are you okay?")
   else
     put "HUH?! SPEAK UP, SONNY!"
 end
-    
-    
+
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
